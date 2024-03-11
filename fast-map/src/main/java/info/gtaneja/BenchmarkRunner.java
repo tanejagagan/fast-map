e0c9d5c1a349ee37288d33f2e9709d92540ad7b8
tanejagagan@yahoo.com <gtaneja@VIJAYSHREEs-MacBook-Pro.local>
2024-03-12 00:17:59 +0530

    FastMap implementation

--- fast-map/src/main/java/info/gtaneja/BenchmarkRunner.java
+++
@@ -1,7 +0,0 @@
-package info.gtaneja;
-
-public class BenchmarkRunner {
-    public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
-    }
-}
--- fast-map/src/main/java/info/gtaneja/Main.java
+++
@@ -1,8 +0,0 @@
-package info.gtaneja;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("This is working good");
-    }
-}
